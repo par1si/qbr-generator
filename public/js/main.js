903c27f03f2bc344a6e628c9180fbc5ee9575cc0
par1si <pparisi13@gmail.com>
2020-04-07 13:30:39 -0700

    update

--- public/js/main.js
+++ public/js/main.js
@@ -1,18 +1,12 @@
 const formFields = document.getElementById('closed-won-deal-input');
 const formFieldsLost = document.getElementById('closed-lost-deal-input');
-const addClosedDealButton = document.getElementById('add-closed-deal-button');
-const addLostDealButton = document.getElementById('add-lost-deal-button');
+const addDealButton = document.getElementById('add-closed-deal-button');
 
-function showFormFieldsElement () {
+
+function showFormFieldsElements () {
     formFields.style.display = 'inline-flex';
-    addClosedDealButton.style.display = 'none';
-}
-
-function showFormFieldsLostElement () {
     formFieldsLost.style.display = 'inline-flex';
-    addLostDealButton.style.display = 'none';
+    addDealButton.style.display = 'none';
 }
 
-
-addClosedDealButton.addEventListener("click", showFormFieldsElement);
-addLostDealButton.addEventListener("click", showFormFieldsLostElement);
+addDealButton.addEventListener("click", showFormFieldsElements);
